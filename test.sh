58a8eb846ed039c4ab43e537d4e967adee6f13ab
SheetJS <dev@sheetjs.com>
2014-03-09 23:02:51 -0400

    added xml files to test suite

--- test.sh
+++ test.sh
@@ -23,12 +23,14 @@ case "$1" in
 	rm -f *.tests
 	;;
 "all") 
-	for i in *.xls *.xlsb *.xlsm *.xlsx; do
+	for i in *.xls *.xlsb *.xlsm *.xlsx *.xml; do
 		echo $i >&2
 		do_test "$i" core "j"
 		#do_test "$i" xlrd "python tests/xlrd.py"
 		#do_test "$i" roo "ruby tests/roo.rb"
 	done
 	grep -- "^- [^0]" *.tests
+	# suppress grep's return value
+	echo
 	;;
 esac
