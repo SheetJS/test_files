a0974ce48b17fdeaf7db5d865b215b98192921c7
SheetJS <dev@sheetjs.com>
2014-03-18 02:40:44 -0400

    updating sheet names

--- tests/sheetnames.sh
+++ tests/sheetnames.sh
@@ -7,7 +7,7 @@ mkdir -p $basedir
 SCPT=./sheetnames.scpt
 if [ ! -e $SCPT ]; then SCPT=./tests/sheetnames.scpt; fi
 
-for i in *.xls *.xlsx; do
+for i in *.xls *.xlsx *.xlsm *.xlsb; do
 	of="$basedir/$i.sheetnames"
 	echo $i $of >&2
 	if [[ ! -s "$of" && ! -e "./tests/skips/$i.skip" ]]; then osascript -s o $SCPT "$i" 2>"$of"; sleep 1; fi
