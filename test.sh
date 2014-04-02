5591611cebf7454da2eab8fbaacbbb7f687cacb5
SheetJS <dev@sheetjs.com>
2014-04-02 16:46:49 -0700

    tests now have a strict 1 minute time limit

--- test.sh
+++ test.sh
@@ -6,10 +6,12 @@
 #   test.sh clean # removes test logs
 #   test.sh all # runs all tests
 # logs are dumped in *.tests
+
+timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
  
 do_test() {
 	echo "# $1" >>"$2".tests
-	$3 "$1" >/dev/null 2>>"$2".tests
+	timeout 60 $3 "$1" >/dev/null 2>>"$2".tests
 	echo "- $? $1" >>"$2".tests
 }
 
@@ -24,6 +26,7 @@ case "$1" in
 	;;
 "all") 
 	for i in *.xls *.xlsb *.xlsm *.xlsx *.xml; do
+		if [[ $(grep -qx "$i" tests.skip) -gt 0 ]]; then continue; fi
 		echo $i >&2
 		do_test "$i" core "j"
 		#do_test "$i" xlrd "python tests/xlrd.py"
