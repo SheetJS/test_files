5769e143a8ba1ec79504229ea1c29a8a623c43d2
SheetJS <dev@sheetjs.com>
2014-07-04 18:42:35 -0400

    added sheetnames baselines

--- tests/sheetnames.sh
+++ tests/sheetnames.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # sheetnames.sh -- generate sheet names using Excel 2011 for Mac
-# Copyright (C) 2013  SheetJS
+# Copyright (C) 2013-2014  SheetJS
 
 basedir=${1:-2011}
 mkdir -p $basedir
 SCPT=./sheetnames.scpt
 if [ ! -e $SCPT ]; then SCPT=./tests/sheetnames.scpt; fi
 
-for i in *.xls *.xlsx *.xlsm *.xlsb; do
+for i in *.xls *.xlsx *.xlsm *.xlsb *.xml; do
 	of="$basedir/$i.sheetnames"
 	echo $i $of >&2
 	if [[ ! -s "$of" && ! -e "./tests/skips/$i.skip" ]]; then osascript -s o $SCPT "$i" 2>"$of"; sleep 1; fi
